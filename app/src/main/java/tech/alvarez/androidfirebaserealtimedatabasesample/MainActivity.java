0edf5e503e5346814bd73b0e28cedf63ed2dd59b
Daniel Alvarez <a3dany@gmail.com>
2016-12-19 22:50:46 -0400

    Initial commit

---
+++ app/src/main/java/tech/alvarez/androidfirebaserealtimedatabasesample/MainActivity.java
@@ -0,0 +1,55 @@
+package tech.alvarez.androidfirebaserealtimedatabasesample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mensajeTextView;
+    private EditText mensajeEditText;
+
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mensajeRef = ref.child("mensaje");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mensajeTextView = (TextView) findViewById(R.id.mensajeTextView);
+        mensajeEditText = (EditText) findViewById(R.id.mensajeEditText);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mensajeRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                mensajeTextView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void modificar(View view) {
+        String mensaje = mensajeEditText.getText().toString();
+        mensajeRef.setValue(mensaje);
+        mensajeEditText.setText("");
+    }
+}
